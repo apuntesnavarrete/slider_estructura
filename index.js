a06c9ef47b2cbf71f1a1843eaa1dd6d786186372
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-09-19 02:33:48 -0500

    terminado

--- index.js
+++ index.js
@@ -9,3 +9,36 @@ let sliderSection = document.querySelectorAll(".slider_section") //contenedores 
  const btnLeft = document.querySelector("#btn-left") //boton rigth
 
  slider.insertAdjacentElement("afterbegin",sliderSectionLast)
+
+ function next(){
+    let sliderSectionfirst = document.querySelectorAll(".slider_section")[0]
+    slider.style.marginLeft = "-200%"
+    slider.style.transition = "all 0.5s"
+    setTimeout(function(){
+        slider.style.transition = "none"
+        slider.insertAdjacentElement("beforeend",sliderSectionfirst)
+        slider.style.marginLeft = "-100%"
+    },500)
+
+ }
+
+
+ function before(){
+    let sliderSectionfirst = document.querySelectorAll(".slider_section")[0]
+    slider.style.marginLeft = "0"
+    slider.style.transition = "all 0.5s"
+    setTimeout(function(){
+        slider.style.transition = "none"
+        slider.insertAdjacentElement("beforeend" ,sliderSectionfirst)
+        slider.style.marginLeft = "-100%"
+    },500)
+
+ }
+
+
+ btnRigth.addEventListener("click",function(){
+    next()
+ })
+ btnLeft.addEventListener("click",function(){
+    before()
+ })
